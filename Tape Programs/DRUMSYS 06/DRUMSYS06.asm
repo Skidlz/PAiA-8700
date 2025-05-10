61b1b89a9210be574130d178a150154bef7222bf
Zack Nelson <zacknelson1@gmail.com>
2025-05-10 14:04:33 -0400

    Updating Drumsys and Quash Test

--- Tape Programs/DRUMSYS 06/DRUMSYS06.asm
+++ Tape Programs/DRUMSYS 06/DRUMSYS06.asm
@@ -31,7 +31,7 @@ CASS	= $0EAA
 ;    S-TABLE (CONTROL CODES)
 ;00E1
 ;---------------------
-STBL	=$00D1
+STBL	= $00D1
 ;---------------------
 ;00E0
 ;    DRUM SIGNATURES
@@ -48,152 +48,152 @@ CRL		db	$6A, $9D, $8D, $87, $7C, $B2, $B9, $CD, $00, $08, $20
 		org $10ED
 PARM	db	$FF
 		org $10F6
-PAR1	db $F6, $F7, $F8, $F9, $FA, $FB, $FC, $FD, $FF, $00
+PAR1	db	$F6, $F7, $F8, $F9, $FA, $FB, $FC, $FD, $FF, $00
 ;*********************************
 		org $1000
 ;*********************************
 ;
-SPHK	lda	#$86	;SPARE HOOK KEYS 8-F3
-		sta	<ACTN+01;USED ONLY TO RE-START
-		nop			;SYTEM.  IN LATER VERSIONS
-		nop			;WILL PROVIDE ADDITTIONAL
-;					 FEATURES
+SPHK	lda	#$86		;SPARE HOOK KEYS 8-F3
+		sta	<lo ACTN+01	;USED ONLY TO RE-START
+		nop				;SYTEM.  IN LATER VERSIONS
+		nop				;WILL PROVIDE ADDITTIONAL
+;						 FEATURES
 ;
-STAR	lda	#$00	;PREPARE ACCUMULATOR AND
-		sta	<PNTR	;ZERO SCORE POINTER
- 		sta	DSP		;AND DISPLAYS
-		tax			;PREPARE X REG AS POINTER
-SLP0	sta	SCOR,x	;AND USE IT TO CLEAR SCORE
+STAR	lda	#$00		;PREPARE ACCUMULATOR AND
+		sta	<lo PNTR	;ZERO SCORE POINTER
+ 		sta	DSP			;AND DISPLAYS
+		tax				;PREPARE X REG AS POINTER
+SLP0	sta	SCOR,x		;AND USE IT TO CLEAR SCORE
 		inx
-		bne	SLP0	;LOOP UNTIL DONE
-SLP1	jsr	RDKY	;GO READ THE KEYBOARD, ETC.
-		bcs	ACTN	;AND IF NO NEW KEYS, BRAND
-		cmp	#$10	;NEW KEY - A "CONTROL" KEY?
-		bcs	CTRL	;YES - BRANCH TO CONTROL
-		cmp	#$08	;ONE OF "SPARE" KEYS?
-		bcs	SPHK	;YES- BRANCH
+		bne	SLP0		;LOOP UNTIL DONE
+SLP1	jsr	RDKY		;GO READ THE KEYBOARD, ETC.
+		bcs	ACTN		;AND IF NO NEW KEYS, BRAND
+		cmp	#$10		;NEW KEY - A "CONTROL" KEY?
+		bcs	CTRL		;YES - BRANCH TO CONTROL
+		cmp	#$08		;ONE OF "SPARE" KEYS?
+		bcs	SPHK		;YES- BRANCH
 ; 		**
-NTRY	lda	#$86	;DRUM ENTRY MODE, GET LINK
-		sta	<ACTN+01
- 		lda	DSIG,y
-		ldx	<PNTR
- 		sta	SCOR,x
- 		jsr	PLAY
- 		jmp	SLP1
-CTRL	lda	STBL,y
-		sta	<ACTN+01
-ACTN	jsr	DUMY
- 		jmp	SLP1
+NTRY	lda	#$86		;DRUM ENTRY MODE, GET LINK
+		sta	<lo ACTN+01	;SET LINK
+ 		lda	DSIG,y		;GET DRUM SIGNATURE
+		ldx	<lo PNTR	;GET SCORE POINTER
+ 		sta	SCOR,x		;SAVE DRUM SIG IN SCORE
+ 		jsr	PLAY		;PLAY THE DRUM BEAT
+ 		jmp	SLP1		;LOOP FOR MORE
+CTRL	lda	STBL,y		;GET COMMAND ADDRESS LINK
+		sta	<lo ACTN+01;AND SET LINK IN JSR DUMY
+ACTN	jsr	DUMY		;AND GO TO COMMAND SUBROUTINE
+ 		jmp	SLP1		;THEN LOOP FOR MORE
 ;
 ;PLAY SUBROUTINE
 ;
-PLAY	ldy	<EXP
-		sta	OUTP
-		and	#$7F
-PLA0	dey
-		bne	PLA0
- 		sta	OUTP
-		inc	<PNTR
-		ldx	<PNTR
- 		stx	DSP
-		rts
+PLAY	ldy	<lo EXP		;GET EXPRESSION VARIABLE
+		sta	OUTP		;OUTPUT CONTROL TO EK-2
+		and	#$7F		;RESET STROBE BIT
+PLA0	dey				;DELAY FOR THE EXP. TIME
+		bne	PLA0		;LOOP UNTIL DONE
+ 		sta	OUTP		;AND TURN DRUM "OFF"
+		inc	<lo PNTR	;INCREMENT SCORE POINTER
+		ldx	<lo PNTR	;PLACE IN X REGISTER
+ 		stx	DSP			;AND SHOW IN DISPLAYS
+		rts				;THEN RETURN
 ;
 ;READ KEY-ALSO IMPORTANT TO TEMPO
 ;
-RDKY	jsr	DECD
-		bcs	DLY
+RDKY	jsr	DECD		;PIEBUG KEYBOARD SUBROUTINE
+		bcs	DLY			;SAME KEY - JUST DELAY
 		ldx	#$00
-		stx	<CNTR
+		stx	<lo CNTR	;ZERO TEMPO COUNTER
 		rts
-DLY		ldx	#$20
-NXTX	ldy	#$3F
-DELY	dey
+DLY		ldx	#$20		;SET X AN Y REGISTER
+NXTX	ldy	#$3F		;DELAY PARAMETERS
+DELY	dey				;AND DO DELAY
 		bne	DELY
 		dex
-		bne	NXTX
-		inc	<CNTR
-		rts
+		bne	NXTX		;LOOP UNTIL DONE
+		inc	<lo CNTR	;INCREMENT TEMPO COUNTER
+		rts				;AND RETURN
 ;
 ;RUN SUBROUTINE
 ;
 ;		**
-RUN		lda	#$7C		;WAIT pointer
-		sta	lo ACTN+01
-CYCL	lda	#$00
-		sta	<PNTR
-CONT	ldx	<PNTR
- 		lda	SCOR,x
-		beq	CYCL
-		jsr	PLAY
-		lda	<CNTR
-		eor	<TMPO
-		bne	RETN
-		sta	<CNTR
-		beq	CONT
-RETN	rts
+RUN		lda	#lo WAIT	;COMMAND LINK TO "WAIT"
+		sta	<lo ACTN+01	;SET COMMAND LINK
+CYCL	lda	#$00		;PREPARE AND SET
+		sta	<lo PNTR	;SCORE POINTER TO 0
+CONT	ldx	<lo PNTR	;GET CURRENT SCORE POINTER
+ 		lda	SCOR,x		;GET CURRENT DRUM SIGNATURE
+		beq	CYCL		;ZERO, END OF SCORE-BRANCH
+		jsr	PLAY		;GO PLAY DRUM SOUND, ETC.
+WAIT	lda	<lo CNTR	;GET TEMPO COUNTER AND
+		eor	<lo TMPO	;COMPARE TO TEMPO VARIABLE
+		bne	RETN		;IF NOT TIMED OUT, RETURN
+OCNT	sta	<lo CNTR	;TIMED OUT - ZERO COUNTER
+		beq	CONT		;BRANCH ALWAYS TO PLAY, ETC
+RETN	rts				;RETURN
 ;
 ;SINGLE STEP SUBROUTINE
 ;
 ;		**
-STEP	lda	#$86		;RETN pointer
-		sta	lo ACTN+01
-		bne	CONT
+STEP	lda	#lo RETN	;COMMAND LINK TO "RETN"
+		sta	<lo ACTN+01	;SET COMMAND LINK
+		bne	CONT		;BRANCH ALWAYS TO PLAY, ETC.
 ;
 ;BACKSPACE SUBROUTINE
 ;
 ;		**
-BACK	lda	#$96		;NEXT pointer
-		sta	lo ACTN+01
-		dec	<PNTR
-		bne	CONT
-		rts
+BACK	lda	#lo NEXT	;COMMAND LINK TO "NEXT"
+		sta	<lo ACTN+01	;SET COMMAND LINK
+		dec	<lo PNTR	;SCORE POINTER BACK ONE
+		bne	CONT		;GO PLAY SCORE, ETC.
+		rts				;AND RETURN
+;		**
+NEXT	lda	#lo RETN	;COMMAND LINK TO "RETN"
+		sta	<lo ACTN+01	;SET COMMAND LINK
+		dec	<lo PNTR	;SCORE POINTER BACK ONE
+		rts				;RETURN
 ;
-NEXT	lda	#$86		;RETN pointer
-		sta	lo ACTN+01
-		dec	<PNTR
-		rts
+;TEMPO
 ;
-;TEMPO;
-;
-;
-TMP		lda	#$A5		;NXT2 pointer
-		sta	lo ACTN+01
-		lda	#$00
-		sta	<TMPO
-NXT2	inc <TMPO
-		rts
+;		**
+TMP		lda	#lo NXT2	;COMMAND LINK TO "NXT2"
+		sta	<lo ACTN+01	;SET COMMAND LINK
+		lda	#$00		;INITIALIZE TEMPO COUNTER
+		sta	<lo TMPO	;AND START COUNTIN
+NXT2	inc <lo TMPO	;UNTIL NEXT COMMAND
+		rts				;RETURN
 ;
 ;SET UP FOR TAPE TRANSFER
 ;
-STTP	ldx	#$07
-STP		lda	$D2,x ;TAPE
-		sta	BUFF,x
-		dex
-		bne	STP
-		rts
+STTP	ldx	#$07		;TRANSFER SEVEN BYTES
+STP		lda	<lo TAPE,x 	;GET PARAMETER
+		sta	BUFF,x		;PLACE PARAMETER
+		dex				;POINT TO NEXT
+		bne	STP			;LOOP UNTIL ALL TRANSFERED
+		rts				;THEN RETURN
 ;
 ;TAPE IN AND OUT ROUTINES
 ;
-TOUT	jsr	STTP
-		lda	#$DD
-		bne	DO
-TIN		jsr	STTP
-		lda	#$11
-DO		jsr	SNBT
- 		jsr	CASS
-		;		**
-		lda	#$86		;RETN pointer
-		sta	lo ACTN+01
-		clc
- 		jsr	BEEP
-		rts
+TOUT	jsr	STTP		;SET UP PARAMETERS
+		lda	#$DD		;SET DUMP "SWITCH"
+		bne	DO			;BRANCH ALWAYS
+TIN		jsr	STTP		;SET UP PARAMETERS
+		lda	#$11		;SET LOAD "SWITCH"
+DO		jsr	SNBT		;TURN ON RELAYS
+ 		jsr	CASS		;DO CASSETTE ROUTINE
+;		**
+		lda	#lo RETN	;COMMAND LINK TO "RETN"
+		sta	<lo ACTN+01	;SET LINK
+		clc				;PREPARE FOR BEEP
+ 		jsr	BEEP		;TURN OFF RELAYS AND BEEP
+		rts				;AND RETURN
 ;
 ;STROBE DRUM EFFECT
 ;
-STRB	dec	<PNTR
- 		jmp	CONT
-				;
-				;
-				;
-				;
-				end
+STRB	dec	<lo PNTR	;PREPARE TO GET SAME DRUM
+ 		jmp	CONT		;PLAY DRUM
+;
+;
+;
+;
+	end
--- Tape Programs/QUASH TEST/QUASH TEST.asm
+++ Tape Programs/QUASH TEST/QUASH TEST.asm
@@ -3,18 +3,14 @@ DISP	=$0820
 SH		=$09EF
 
 	org $0000
-TEST
-	lda #$10		; set up note
-REPEAT
-	ldx #$10		; set up S/H pointer
-LOOP1
-	sta SH,x	; update S/H
-	stx DISP	; show in display
-	ldy #$10
-LOOP2
-	dey			; delay
-	bne LOOP2
-	dex			; point to next S/H
-	bne LOOP1	; if not done, loop
-	beq REPEAT	; branch always
-	end
+TEST	lda #$10	; set up note
+REPEAT	ldx #$10	; set up S/H pointer
+LOOP1	sta SH,x	; update S/H
+		stx DISP	; show in display - working
+		ldy #$10	; set up delay
+LOOP2	dey			; delay
+		bne LOOP2
+		dex			; point to next S/H
+		bne LOOP1	; if not done, loop
+		beq REPEAT	; branch always repeat
+		end
